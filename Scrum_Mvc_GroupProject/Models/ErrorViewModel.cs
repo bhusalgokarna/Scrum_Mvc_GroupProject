33760382f6ef7eb074e72e81073233f8ff4f5bda
bhusalgokarna <114413903+bhusalgokarna@users.noreply.github.com>
2023-06-20 10:35:25 +0200

    Add project files.

---
+++ Scrum_Mvc_GroupProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Scrum_Mvc_GroupProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
