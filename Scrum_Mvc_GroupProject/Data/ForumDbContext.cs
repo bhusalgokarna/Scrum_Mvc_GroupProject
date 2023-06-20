612939df6447ec8ff856a8e984cbd17a52c3d455
bhusalgokarna <114413903+bhusalgokarna@users.noreply.github.com>
2023-06-20 13:26:49 +0200

    Dbset Gemaakt

--- Scrum_Mvc_GroupProject/Data/ForumDbContext.cs
+++ Scrum_Mvc_GroupProject/Data/ForumDbContext.cs
@@ -10,6 +10,6 @@ namespace Scrum_Mvc_GroupProject.Data
         }
         public DbSet<ForumCategory> ForumCategories { get; set; }
         public DbSet<DiscussieThread> DiscussieThreads { get; set; }
-        public DbSet<Reactie>Reacties { get; set; }
+        public DbSet<Reactie> Reacties { get; set; }
     }
 }
