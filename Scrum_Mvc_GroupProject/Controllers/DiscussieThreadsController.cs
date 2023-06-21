bdda98d99d65e76c5eb7757816a58cab513245e3
Intec <areejalsaqer93@gmail.com>
2023-06-21 09:36:10 +0200

    controller added

---
+++ Scrum_Mvc_GroupProject/Controllers/DiscussieThreadsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Scrum_Mvc_GroupProject.Controllers
+{
+    public class DiscussieThreadsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
